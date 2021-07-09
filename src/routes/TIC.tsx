0d91df5310f729a592494def24693a246eaf41b7
Orion31Dev <orion31dev@gmail.com>
2021-07-09 12:38:24 -0400

    user input on TIC page

--- src/routes/TIC.tsx
+++ src/routes/TIC.tsx
@@ -10,6 +10,17 @@ function TIC() {
     <div className="tic">
       <Header />
       <TICInfo id={ticId} />
+      <div className="user-input section">
+        <select>
+          <option value="FP">FP</option>
+          <option value="PC">PC</option>
+        </select>
+        <div className="input-wrapper">
+          <input type="text" />
+          <div className="label">Comments</div>
+        </div>
+        <div className="button">Submit</div>
+      </div>
     </div>
   );
 }
